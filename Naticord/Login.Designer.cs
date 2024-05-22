3f5fa06ef032243f40baf390169055f1be38e64c
FragAverage <54283228+FragAverage@users.noreply.github.com>
2024-05-22 14:38:46 +0100

    change: Login form elements stay centered correctly and apply minimum size to login form

--- Naticord/Login.Designer.cs
+++ Naticord/Login.Designer.cs
@@ -29,34 +29,19 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
-            this.emailTextBox = new System.Windows.Forms.TextBox();
-            this.emailPasswordLoginButton = new System.Windows.Forms.Button();
+            this.MinimumSize = new System.Drawing.Size(381, 377);
             this.usernameLabel = new System.Windows.Forms.Label();
-            this.tokenLabel = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginFormPanel = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.Email = new System.Windows.Forms.Label();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.emailPasswordLoginButton = new System.Windows.Forms.Button();
+            this.loginFormPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
-            // 
-            // emailTextBox
-            // 
-            this.emailTextBox.Location = new System.Drawing.Point(50, 207);
-            this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.Size = new System.Drawing.Size(260, 20);
-            this.emailTextBox.TabIndex = 0;
-            // 
-            // emailPasswordLoginButton
-            // 
-            this.emailPasswordLoginButton.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailPasswordLoginButton.Location = new System.Drawing.Point(139, 279);
-            this.emailPasswordLoginButton.Name = "emailPasswordLoginButton";
-            this.emailPasswordLoginButton.Size = new System.Drawing.Size(75, 27);
-            this.emailPasswordLoginButton.TabIndex = 1;
-            this.emailPasswordLoginButton.Text = "Login";
-            this.emailPasswordLoginButton.UseVisualStyleBackColor = true;
-            this.emailPasswordLoginButton.Click += new System.EventHandler(this.emailPasswordLoginButton_Click);
             // 
             // usernameLabel
             // 
@@ -66,84 +51,119 @@
             this.usernameLabel.Size = new System.Drawing.Size(0, 13);
             this.usernameLabel.TabIndex = 2;
             // 
-            // tokenLabel
+            // loginFormPanel
             // 
-            this.tokenLabel.AutoSize = true;
-            this.tokenLabel.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tokenLabel.Location = new System.Drawing.Point(34, 151);
-            this.tokenLabel.Name = "tokenLabel";
-            this.tokenLabel.Size = new System.Drawing.Size(298, 17);
-            this.tokenLabel.TabIndex = 3;
-            this.tokenLabel.Text = "Sign into Discord using your email and password.";
+            this.loginFormPanel.Controls.Add(this.emailPasswordLoginButton);
+            this.loginFormPanel.Controls.Add(this.label1);
+            this.loginFormPanel.Controls.Add(this.passwordTextBox);
+            this.loginFormPanel.Controls.Add(this.Email);
+            this.loginFormPanel.Controls.Add(this.emailTextBox);
+            this.loginFormPanel.Controls.Add(this.label2);
+            this.loginFormPanel.Controls.Add(this.pictureBox2);
+            this.loginFormPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.loginFormPanel.Location = new System.Drawing.Point(0, 0);
+            this.loginFormPanel.Name = "loginFormPanel";
+            this.loginFormPanel.Size = new System.Drawing.Size(365, 338);
+            this.loginFormPanel.TabIndex = 10;
             // 
-            // pictureBox1
+            // pictureBox2
             // 
-            this.pictureBox1.Image = global::Naticord.Properties.Resources.logo;
-            this.pictureBox1.Location = new System.Drawing.Point(127, 38);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(97, 99);
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox2.Image = global::Naticord.Properties.Resources.logo;
+            this.pictureBox2.Location = new System.Drawing.Point(132, 33);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(97, 99);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
             // 
-            // passwordTextBox
+            // label2
             // 
-            this.passwordTextBox.Location = new System.Drawing.Point(52, 251);
-            this.passwordTextBox.Name = "passwordTextBox";
-            this.passwordTextBox.Size = new System.Drawing.Size(260, 20);
-            this.passwordTextBox.TabIndex = 7;
-            this.passwordTextBox.UseSystemPasswordChar = true;
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(298, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Sign into Discord using your email and password.";
             // 
             // Email
             // 
+            this.Email.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Email.AutoSize = true;
             this.Email.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Email.Location = new System.Drawing.Point(47, 187);
+            this.Email.Location = new System.Drawing.Point(50, 163);
             this.Email.Name = "Email";
             this.Email.Size = new System.Drawing.Size(39, 17);
-            this.Email.TabIndex = 8;
+            this.Email.TabIndex = 10;
             this.Email.Text = "Email";
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.emailTextBox.Location = new System.Drawing.Point(50, 183);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(260, 20);
+            this.emailTextBox.TabIndex = 9;
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(47, 230);
+            this.label1.Location = new System.Drawing.Point(50, 206);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 17);
-            this.label1.TabIndex = 9;
+            this.label1.TabIndex = 12;
             this.label1.Text = "Password";
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.passwordTextBox.Location = new System.Drawing.Point(50, 227);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(260, 20);
+            this.passwordTextBox.TabIndex = 11;
+            this.passwordTextBox.UseSystemPasswordChar = true;
+            // 
+            // emailPasswordLoginButton
+            // 
+            this.emailPasswordLoginButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.emailPasswordLoginButton.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.emailPasswordLoginButton.Location = new System.Drawing.Point(143, 267);
+            this.emailPasswordLoginButton.Name = "emailPasswordLoginButton";
+            this.emailPasswordLoginButton.Size = new System.Drawing.Size(75, 27);
+            this.emailPasswordLoginButton.TabIndex = 13;
+            this.emailPasswordLoginButton.Text = "Login";
+            this.emailPasswordLoginButton.UseVisualStyleBackColor = true;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(365, 338);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Email);
-            this.Controls.Add(this.passwordTextBox);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.tokenLabel);
             this.Controls.Add(this.usernameLabel);
-            this.Controls.Add(this.emailPasswordLoginButton);
-            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.loginFormPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Login";
             this.Text = "Naticord Login";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.loginFormPanel.ResumeLayout(false);
+            this.loginFormPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox emailTextBox;
-        private System.Windows.Forms.Button emailPasswordLoginButton;
         private System.Windows.Forms.Label usernameLabel;
-        private System.Windows.Forms.Label tokenLabel;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Panel loginFormPanel;
+        private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button emailPasswordLoginButton;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox passwordTextBox;
     }
 }
