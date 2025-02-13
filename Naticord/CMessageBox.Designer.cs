6d7c934a28134bfe8590901fe88529c4f0f4b917
patricktbp <135556230+n1d3v@users.noreply.github.com>
2025-02-13 14:25:45 +0000

    Set BackColor to white on CMessageBox.

--- Naticord/CMessageBox.Designer.cs
+++ Naticord/CMessageBox.Designer.cs
@@ -81,6 +81,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(484, 124);
             this.Controls.Add(this.okButton);
             this.Controls.Add(this.messageContent);
