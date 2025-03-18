5a98421acaa8d8577d736bb1f49ce2b040eed918
patricktbp <135556230+n1d3v@users.noreply.github.com>
2025-03-18 01:28:24 +0000

    Implement using the Hilight registry value as FSControl's background color.

--- Naticord/FSControl.cs
+++ Naticord/FSControl.cs
@@ -1,13 +1,19 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace Naticord
 {
     public partial class FSControl : UserControl
     {
+        private Color HighlightColor;
+
         public FSControl()
         {
             InitializeComponent();
+            HighlightColor = GetHighlightColor();
         }
 
         public void ClickedDesignChange(bool clicked)
@@ -15,7 +21,7 @@ namespace Naticord
             if (clicked)
             {
                 nameLabel.ForeColor = Color.White;
-                this.BackColor = Color.MediumPurple;
+                this.BackColor = HighlightColor;
             }
             else
             {
@@ -40,6 +46,25 @@ namespace Naticord
         {
             get => profilePictureItem.Image;
             set => profilePictureItem.Image = value;
+        }
+
+        private Color GetHighlightColor()
+        {
+            try
+            {
+                using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Colors"))
+                {
+                    string colorString = key?.GetValue("Hilight") as string;
+                    if (!string.IsNullOrEmpty(colorString))
+                    {
+                        int[] rgb = colorString.Split(' ').Select(int.Parse).ToArray();
+                        return Color.FromArgb(rgb[0], rgb[1], rgb[2]);
+                    }
+                }
+            }
+            catch { }
+
+            return Color.FromArgb(0, 120, 215); // Default 'Windows 10' Hilight color
         }
     }
 }
