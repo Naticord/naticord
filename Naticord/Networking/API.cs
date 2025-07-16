8f147b028ef26cae8149a8bb96ea0252a5d8734a
patricktbp <135556230+n1d3v@users.noreply.github.com>
2025-07-16 17:45:22 +0100

    Revert "Switch out the 'Legacy' icons for more fitting ones"

--- Naticord/Networking/API.cs
+++ Naticord/Networking/API.cs
@@ -14,6 +14,7 @@ namespace Naticord.Networking
 
         // Configuration (Firefox 137 on Windows 10)
         private static readonly string XSuperProperties = "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRmlyZWZveCIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJlbi1VUyIsImhhc19jbGllbnRfbW9kcyI6ZmFsc2UsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChXaW5kb3dzIE5UIDEwLjA7IFdpbjY0OyB4NjQ7IHJ2OjEzNy4wKSBHZWNrby8yMDEwMDEwMSBGaXJlZm94LzEzNy4wIiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTM3LjAiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6IiIsInJlZmVycmluZ19kb21haW4iOiIiLCJyZWZlcnJlcl9jdXJyZW50IjoiIiwicmVmZXJyaW5nX2RvbWFpbl9jdXJyZW50IjoiIiwicmVsZWFzZV9jaGFubmVsIjoic3RhYmxlIiwiY2xpZW50X2J1aWxkX251bWJlciI6Mzg2NDMyLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ==";
+
         private static readonly string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0";
 
         static API()
